92c0dd6834eac195ac548474e8ce4476ace99cea
Ethan Shepherd <ethan.m.shepherd@gmail.com>
2022-06-13 20:35:00 -0700

    started initial vue components working on server auth

--- server/ranger-server.js
+++ server/ranger-server.js
@@ -13,11 +13,33 @@ const dotenv = require("dotenv");
 // get config vars
 dotenv.config();
 
-// access config var
-process.env.TOKEN_SECRET;
+const generateAccessToken = (username) => {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+};
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 app.get("/", async (req, res) => {
   res.json({ response: "ok" });
+});
+
+app.post("/api/upload", authenticateToken, (req, res) => {
+  res.json({ response: "upload" });
+});
+
+app.post("/api/signin", (req, res) => {
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token);
 });
 
 app.listen(PORT, () => {
---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row">
+    <input v-model.number="num1" type="number" />
+    <span>+</span>
+    <input v-model.number="num2" type="number" />
+    <span>=</span>
+    <span>{{ num1 + num2 }}</span>
+  </div>
+  <div class="row second">
+    <select v-model="name">
+      <option>Carne Asada</option>
+      <option>Pollo</option>
+      <option>Bean</option>
+      <option>Al Pastor</option>
+    </select>
+    <p v-if="name">My favorite kind of taco is {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      name: "Vue",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$primary: #5968d7;
+
+.row {
+  margin: 50px;
+  display: flex;
+  justify-content: center;
+  font-family: "Work Sans", sans-serif;
+}
+
+input,
+span {
+  padding: 10px;
+  font-size: 30px;
+  font-family: "Work Sans", sans-serif;
+}
+
+input {
+  width: 50px;
+}
+
+span {
+  width: 20px;
+}
+
+.second {
+  font-size: 18px;
+  p {
+    margin-left: 20px;
+  }
+}
+</style>
